9c2921a1654410ff12cf227b2757fb73014eb2ff
Stan Wijckmans <stanw@aviobook.aero>
2022-09-02 21:35:04 +0200

    Initial app

---
+++ AvaloniaDelayTest/MainWindow.axaml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+
+namespace AvaloniaDelayTest
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartTimer();
+        }
+
+        private async void StartTimer()
+        {
+            while (true)
+            {
+                var startTime = DateTime.Now;
+
+                await Task.Delay(250);
+
+                var duration = DateTime.Now - startTime;
+                if (duration > TimeSpan.FromMilliseconds(500))
+                {
+                    Console.WriteLine($"Too long: {duration}");
+                }
+            }
+        }
+    }
+}
